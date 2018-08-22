3fc478617e0c19b0dfd3c933c4bf63add3538434
Ravi Agrawal <ravi.agrwl420@gmail.com>
2018-08-21 20:29:35 -0700

    Refactor population.cpp

--- src/population.cpp
+++ src/population.cpp
@@ -1,15 +1,18 @@
 #include<population.h>
 
 // Initialize static variables with default values
+// Initialize a default random number generator
 mt19937 Population::rng = mt19937(random_device()());
+
+// Initialize a uniform random distribution [0, 1]
 uniform_real_distribution<> Population::dist = uniform_real_distribution<>(0, 1);
 
-// Initialize the random number generator
+// Initialize a random number generator with a seed
 void Population::initialize (int seed) {
 	rng = mt19937(seed);
 }
 
-// Initialize a population of size m with chromosomes of length n
+// Initialize a random population of size m with chromosomes of length n
 Population::Population (int m, int n) {
 	size = m;
 	chromosome_length = n;
@@ -35,7 +38,7 @@ void Population::setFitnessFunc (function<float(Chromosome)> f) {
 	fitnessFunc = f;
 }
 
-// Compute fitness for the population
+// Compute fitness for the whole population
 void Population::computeFitness () {
 	for (Chromosome &individual: individuals) {
 		individual.computeFitness(fitnessFunc);
@@ -51,7 +54,7 @@ void Population::sortByFitness () {
 	sort(individuals.begin(), individuals.end());
 }
 
-// Add mutation
+// Implements mutation which replaces the least fit population with random chromosomes
 void Population::mutation (float mutationRatio) {
 	int numMutation = mutationRatio * size;
 
@@ -60,7 +63,7 @@ void Population::mutation (float mutationRatio) {
 	}
 }
 
-// Generate new population by crossover
+// Implements crossover which crosses a random elite chromosome with a random chromosome from the population
 void Population::crossover (float eliteRatio, float mutationRatio, float eliteBias, TYPE type) {
 	int numElite = eliteRatio * size;
 	int numMutation = mutationRatio * size;
@@ -94,19 +97,20 @@ Population Population::nextGeneration (float eliteRatio, float mutationRatio, fl
 	// Make a copy
 	Population copy = Population(*this);
 
-	// Add random solutions at the end
+	// Perform mutation and crossover on the population
 	copy.mutation(mutationRatio);
 	copy.crossover(eliteRatio, mutationRatio, eliteBias, type);
 
 	return copy;
 }
 
+// Get the best individual from the population
 Chromosome Population::getBest () {
 	this->sortByFitness();
 	return individuals[0];
 }
 
-// Print population
+// Print the best fitness of the population
 void Population::print () {
-	cout << "Fitness: " << individuals[0].getFitness() << endl << endl;
+	cout << "Fitness: " << getBest().getFitness() << endl << endl;
 }
